2dfc6e9ec9d9933b0dafb61af38f9fb3d74c4e15
Bittersweet <zhouziqun@cool2645.com>
2019-12-05 15:48:47 +0800

    :bug: update user's v2ray info in checkUser

--- server/manager.go
+++ server/manager.go
@@ -32,25 +32,58 @@ func (u *UserManager) check() error {
 
 func (u *UserManager) checkUser(user musdk.User) error {
 	var err error
-	if user.IsEnable() && !u.Exist(user) {
-		// run user
-		exist, err := u.vm.AddUser(&user.V2rayUser)
-		if err != nil {
-			logger.Errorf("add user %s error %v", user.V2rayUser.UUID, err)
-			return err
+	if user.IsEnable() {
+		if !u.Exist(user) {
+			logger.Infof("add user id %d uuid %s", user.Id, user.V2rayUser.UUID)
+			// add user
+			exist, err := u.vm.AddUser(&user.V2rayUser)
+			if err != nil {
+				logger.Errorf("add user %s error %v", user.V2rayUser.UUID, err)
+				return err
+			}
+			if exist {
+				logger.Infof("user %s already exist", user.V2rayUser.UUID)
+			}
+			u.AddUser(user)
+			return nil
+		} else {
+			// update user info
+			oldUser, ok := u.GetUser(user.GetId())
+			if ok {
+				// check if user info needs update
+				if oldUser.V2rayUser.GetAlterID() != user.V2rayUser.GetAlterID() ||
+					oldUser.V2rayUser.GetEmail() != user.V2rayUser.GetEmail() ||
+					oldUser.V2rayUser.GetLevel() != user.V2rayUser.GetLevel() ||
+					oldUser.V2rayUser.GetUUID() != user.V2rayUser.GetUUID() {
+					logger.Infof("update user id %d old uuid %s uuid %s",
+						user.GetId(), oldUser.V2rayUser.GetUUID(), user.V2rayUser.GetUUID())
+					// do update, first remove then add
+					// remove old user
+					err = u.vm.RemoveUser(&oldUser.V2rayUser)
+					if err != nil {
+						logger.Errorf("remove user error %v", err)
+						time.Sleep(time.Second * 10)
+						return err
+					}
+					u.RemoveUser(oldUser)
+
+					// add new user
+					_, err := u.vm.AddUser(&user.V2rayUser)
+					if err != nil {
+						logger.Errorf("add user %s error %v", user.V2rayUser.UUID, err)
+						return err
+					}
+					u.AddUser(user)
+					return nil
+				}
+			}
 		}
-		if !exist {
-			logger.Errorf("add user %s success", user.V2rayUser.UUID)
-		}
-		u.AddUser(user)
-		return nil
 	}
 
 	if !user.IsEnable() && u.Exist(user) {
 		logger.Infof("stop user id %d uuid %s", user.Id, user.V2rayUser.UUID)
 		// stop user
 		err = u.vm.RemoveUser(&user.V2rayUser)
-
 		if err != nil {
 			logger.Errorf("remove user error %v", err)
 			time.Sleep(time.Second * 10)
